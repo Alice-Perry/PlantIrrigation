4d64de04e568404e480fb4ad0613d59f3b736bfe
Alice <alice@perry.nl>
2021-05-25 23:08:20 +0200

    class wordt java en plant String naar int

--- Program.java
+++ Program.java
@@ -2,40 +2,47 @@ import java.util.concurrent.TimeUnit;
 import java.util.Scanner;
 
 public class Program {
-    public void HoursBeforeWateringAgain() {
-        int hours = WateringProgramPerPlant.hoursBeforeWateringAgain;
-        try {
-            TimeUnit.HOURS.sleep(hours);
-        } catch (InterruptedException e) {
-            System.out.println("Something went wrong." + e);
-        }
+
+    public static void main(String[] args) {
+        
     }
 
-    public int DaysOnVacationInput() {
-        System.out.println("How many days will you be on vacation?");
-        Scanner keyboard = new Scanner(System.in);
-        int daysOnVacation = keyboard.nextInt();
-        keyboard.close();
-        return daysOnVacation;
-    }
 
-    private int amountOfWaterToGive = 50; // in ml
-    private int daysOfWatering = 1;
-    private int flowPerMinuteInMilliliter = 10;
-    private int flowMeter = 0;
-    private boolean openValve = false;
-
-    public void GiveWater() {
-        for (int i = daysOfWatering; i < DaysOnVacationInput(); i++) {
-            openValve = true;
-            flowMeter = flowMeter + flowPerMinuteInMilliliter;
-            if (flowMeter >= amountOfWaterToGive) {
-                openValve = false;
-                System.out.println(amountOfWaterToGive + " ml water has been given to your plant.");
-                System.out.println("Next watering will be in 24 hours.");
-                daysOfWatering++;
-                HoursBeforeWateringAgain();
+        public void HoursBeforeWateringAgain() {
+            int hours = hoursBeforeWateringAgain;
+            try {
+                TimeUnit.HOURS.sleep(hours);
+            } catch (InterruptedException e) {
+                System.out.println("Something went wrong." + e);
             }
         }
-    }
+
+        public int DaysOnVacationInput() {
+            System.out.println("How many days will you be on vacation?");
+            Scanner keyboard = new Scanner(System.in);
+            int daysOnVacation = keyboard.nextInt();
+            keyboard.close();
+            return daysOnVacation;
+        }
+
+        private int amountOfWaterToGive = 50; // in ml
+        private int daysOfWatering = 1;
+        private int flowPerMinuteInMilliliter = 10;
+        private int flowMeter = 0;
+        private boolean openValve = false;
+
+        public void GiveWater() {
+            for (int i = daysOfWatering; i < DaysOnVacationInput(); i++) {
+                openValve = true;
+                flowMeter = flowMeter + flowPerMinuteInMilliliter;
+                if (flowMeter >= amountOfWaterToGive) {
+                    openValve = false;
+                    System.out.println(amountOfWaterToGive + " ml water has been given to your plant.");
+                    System.out.println("Next watering will be in 24 hours.");
+                    daysOfWatering++;
+                    HoursBeforeWateringAgain();
+                }
+            }
+        }
+    
 }
